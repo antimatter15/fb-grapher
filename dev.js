d7d1b04881b3ddee97413314147aa02f35cbd29b
antimatter15 <antimatter15@gmail.com>
2010-11-15 23:15:03 -0500

    should work

--- dev.js
+++ dev.js
@@ -3,80 +3,94 @@ setInterval(function(){
   (new Function(document.querySelector('.uiMorePager a').getAttribute('onclick')))()
 },4000);
 
-    var pad = 50;
 
 
-
-setInterval(function(){
-if(!window.r) return;
-r.clear();
-var els = document.querySelectorAll('.notification, h6, div div div div[data-ft], abbr[data-date]'); //Hopefully generic selectors that will survive fb theme updates
-var posts = [];
-for(var i = els.length; i--; ){
-  var el = els[i];
-  if(el.getAttribute('data-date')){
-    posts.push(new Date(el.getAttribute('data-date')));
-  }else if(el.getAttribute('data-ft') && el.getAttribute('data-ft').indexOf('attach') != -1){
-    //ignoreh
-  }else{
-    posts.push(el.innerText);
+function scrapeData(){
+  var els = document.querySelectorAll('.notification, h6, div div div div[data-ft], abbr[data-date]'); //Hopefully generic selectors that will survive fb theme updates
+  var posts = [];
+  for(var i = els.length; i--; ){
+    var el = els[i];
+    if(el.getAttribute('data-date')){
+      posts.push(new Date(el.getAttribute('data-date')));
+    }else if(el.getAttribute('data-ft') && el.getAttribute('data-ft').indexOf('attach') != -1){
+      //ignoreh
+    }else{
+      posts.push(el.innerText);
+    }
   }
-}
 
-//.replace(/^.+?\s/g,'').indexOf('changed')
-//Names; document.querySelectorAll('a[data-hovercard]')
+  //.replace(/^.+?\s/g,'').indexOf('changed')
+  //Names; document.querySelectorAll('a[data-hovercard]')
 
-var last_time, first_time;
-var groups = {
-  "Friend": /now friends/,
-  "Comment": /commented on/,
-  "Like": /like/,
-  "Changed": /changed/
-};
-var geeky = Object.keys(groups);/*G(roup)KEY*/
-var gct = {};
-var tmp = {};
-var xdata = [];
-var ydata = [];
-for(var i = 0; i < posts.length; i++){
+  var last_time, first_time;
+  var groups = {
+    "Friend": /now friends/,
+    "Comment": /commented on/,
+    "Like": /like/,
+    "Changed": /changed/
+  };
+  var geeky = Object.keys(groups);/*G(roup)KEY*/
+  var gct = {};
+  var tmp = {};
+  var xdata = [];
+  var ydata = [];
+  var ct = 0;
+  for(var k in groups){
+    if(!gct[ct]) gct[ct] = 0;
+    if(!tmp[ct]) tmp[ct] = 0;
+    if(!ydata[ct]) ydata[ct] = [];
+    if(!xdata[ct]) xdata[ct] = [];
+    ct++;
+  }
+  for(var i = 0; i < posts.length; i++){
 
-  if(posts[i].getTime){
-    if(last_time){
-    
-      var dnum = Math.round((last_time-first_time)/1000/60/60/24*100)/100;
-      if(!isNaN(dnum)){
-        var ct = 0;
-        for(var k in groups){
-          if(tmp[ct]){
-            gct[ct] += tmp[ct];
-            ydata[ct].push(gct[ct]);
-            xdata[ct].push(dnum)
+    if(posts[i].getTime){
+      if(last_time){
+      
+        var dnum = Math.round((last_time-first_time)/1000/60/60/24*100)/100;
+        if(!isNaN(dnum)){
+          var ct = 0;
+          for(var k in groups){
+            if(tmp[ct]){
+              gct[ct] += tmp[ct];
+              ydata[ct].push(gct[ct]);
+              xdata[ct].push(dnum)
+            }
+            ct++;
           }
-          ct++;
         }
       }
-    }
-    tmp = {};
-    last_time = posts[i]; 
-    if(!first_time) first_time = posts[i];
-  } else {
-    var post = posts[i].trim().split('\n')[0].replace(/\xb7.*$/,'').replace(/Remove Post/g,'').trim();
-    var ct = 0;
-    console.log(post)
-    for(var k in groups){
-      if(groups[k].test(post)){
-        if(!gct[ct]) gct[ct] = 0;
-        if(!tmp[ct]) tmp[ct] = 0;
-        if(!ydata[ct]) ydata[ct] = [];
-        if(!xdata[ct]) xdata[ct] = [];
-        console.log(k)
-        tmp[ct]++;
-        break;
+      tmp = {};
+      last_time = posts[i]; 
+      if(!first_time) first_time = posts[i];
+    } else {
+      var post = posts[i].trim().split('\n')[0].replace(/\xb7.*$/,'').replace(/Remove Post/g,'').trim();
+      var ct = 0;
+      console.log(post)
+      for(var k in groups){
+        if(groups[k].test(post)){
+
+          console.log(k)
+          tmp[ct]++;
+          break;
+        }
+        ct++;
       }
-      ct++;
     }
   }
+  window.posts = posts;
+  window.ydata = ydata;
+  window.xdata = xdata;
 }
+
+    
+var pad = 50;
+
+
+
+function drawCharts(){
+    r.clear();
+
 
     
     r.canvas.style.backgroundColor = '#EDEFF4';
@@ -95,9 +109,7 @@ for(var i = 0; i < posts.length; i++){
     }, function () {
         this.tags && this.tags.remove();
     });
-},5000);
-
-
+}
 
 
 var scripts = [
@@ -115,6 +127,11 @@ var next = function(){
     document.body.appendChild(s);
   }else{ //duhn!
     window.r = Raphael(pad, pad, innerWidth - 2*pad, 500);
+    
+    setInterval(function(){
+      scrapeData();
+      drawCharts();
+    }, 5000)
   }
 };
 next();
